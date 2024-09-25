e4091d7a9df1372de732ffca7a31b7390918489f
Luxshan-Partheepan-ICD2O1-01 <luxshan.partheepan@student.tdsb.on.ca>
2024-09-25 22:41:01 +0000

    finished

--- PP1_7.py
+++ PP1_7.py
@@ -1,19 +1,35 @@
 
 
-q1():
-  #Write code here
+def q1():
 
-q2():
-  #Write code here
+ print("true")
 
-q3():
-  #Write code here
+def q2():
 
-q4():
-  #Write code here
+  math = input("Input an integer: ")
+  think = int(math)
+  compare = think > 5
+  print(compare)
+  
+  
 
-q5():
-  #Write code here
+def q3():
+  
+  letters = input("Input the letter a")
+  show = letters == 'a'
+  print(show)
+
+def q4():
+  vocabulary = input("Input a word ealier in the dictionary than google: ")
+  search = vocabulary < 'google'
+  print(search)
+
+def q5():
+  num1 = int(input("Input an integer: "))
+  num2 = int(input("Input another integer: "))
+  mathanswer = num1 * num2 > 40
+  print("Your numbers multiplied together are greater than 40:", mathanswer)
+
 
 #Do edit the code below
 #Comment the lines below when running your tests
